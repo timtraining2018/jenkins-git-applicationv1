406a5c1b6a25154621be369fd1c14767590068f9
Timothy Ndunguru <timothy.ndunguru@egainternal.go.tz>
2018-12-23 15:58:54 +0300

    begin

---
+++ src/main/java/JenkinsWithGit_ApplicationFinal/JenkinsWithGit_ApplicationFinal/App.java
@@ -0,0 +1,13 @@
+package JenkinsWithGit_ApplicationFinal.JenkinsWithGit_ApplicationFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
